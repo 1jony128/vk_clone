006f6db543a75287f5a0dd297e443a0a421670b4
128evgeny128 <1jony128@gmail.com>
2022-05-01 22:05:58 +0900

    initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './accept/scss/app.scss'
+
+
+function App() {
+
+
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
